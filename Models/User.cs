115ba1f25a8e3144378937565e37cb72971e4f77
hoang pham <hoang.pm@softworld.jp>
2023-06-26 22:56:42 +0700

    change user

--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,6 @@ namespace QLNH.Models {
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Password1 { get; set; }
     }
 }
