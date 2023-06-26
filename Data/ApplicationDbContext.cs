aa23dae8921f0e071902098c69fa94763d9408bf
hoang pham <hoang.pm@softworld.jp>
2023-06-26 22:55:13 +0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QLNH.Models;
+
+namespace QLNH.Data {
+    public class ApplicationDbContext : DbContext {
+        public DbSet<User> User { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+}
