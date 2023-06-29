c322a02229e3c358d75f680dc167f6dd2ca8f610
hoang pham <phamminhhoangf3@gmail.com>
2023-06-29 21:49:38 +0700

    test

--- Migrations/20230629010103_AddItemDatabase.cs
+++ Migrations/20230629010103_AddItemDatabase.cs
@@ -7,26 +7,26 @@ namespace QLNH.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Items",
-                columns: table => new {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(type: "longtext", nullable: true)
-                        .Annotation("MySql:CharSet", "utf8mb4"),
-                    Description = table.Column<string>(type: "longtext", nullable: true)
-                        .Annotation("MySql:CharSet", "utf8mb4")
-                },
-                constraints: table => {
-                    table.PrimaryKey("PK_Items", x => x.Id);
-                })
-                .Annotation("MySql:CharSet", "utf8mb4");
+            //migrationBuilder.CreateTable(
+            //    name: "Items",
+            //    columns: table => new {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+            //        Name = table.Column<string>(type: "longtext", nullable: true)
+            //            .Annotation("MySql:CharSet", "utf8mb4"),
+            //        Description = table.Column<string>(type: "longtext", nullable: true)
+            //            .Annotation("MySql:CharSet", "utf8mb4")
+            //    },
+            //    constraints: table => {
+            //        table.PrimaryKey("PK_Items", x => x.Id);
+            //    })
+            //    .Annotation("MySql:CharSet", "utf8mb4");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Items");
+            //migrationBuilder.DropTable(
+            //    name: "Items");
         }
     }
 }
