38186f56c92aaf7555d2ed0401e6194452ee7508
hoang pham <phamminhhoangf3@gmail.com>
2023-07-02 22:53:52 +0700

    add swagger

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QLNH.Data;
+using QLNH.Models;
 
 namespace QLNH.Controllers {
+    [ApiController]
+    [Route("[controller]")]
     public class UserController : Controller {
-        public IActionResult Index() {
-            return View();
+        private readonly ApplicationDbContext _context;
+        public UserController(ApplicationDbContext context) {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<User> Get() {
+            return _context.User.ToList();
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,7 @@ namespace QLNH.Models {
     public class User {
         [Key]
         public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Password { get; set; }
     }
 }
